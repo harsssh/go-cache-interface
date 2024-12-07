ee8c929d2e569c351c44a8cd62db46497bb80122
Kentaro Mizuki <66548698+harsssh@users.noreply.github.com>
2024-12-07 18:28:01 +0900

    redis client の wrapper

--- cache.go
+++ cache.go
@@ -2,7 +2,10 @@ package isucon_utility
 
 import (
 	"context"
+	"errors"
+	"github.com/goccy/go-json"
 	lru "github.com/hashicorp/golang-lru/v2"
+	"github.com/redis/go-redis/v9"
 )
 
 type Maybe[V any] struct {
@@ -15,7 +18,6 @@ type Cache[K comparable, V any] interface {
 	Set(ctx context.Context, key K, value V) error
 	Delete(ctx context.Context, key K) error
 	Clear(ctx context.Context) error
-	Len(ctx context.Context) (int, error)
 }
 
 type InMemoryLRUCache[K comparable, V any] struct {
@@ -45,14 +47,62 @@ func (c *InMemoryLRUCache[K, V]) Clear(ctx context.Context) error {
 	return nil
 }
 
-func (c *InMemoryLRUCache[K, V]) Len(ctx context.Context) (int, error) {
-	return c.l.Len(), nil
-}
-
 func NewInMemoryLRUCache[K comparable, V any](size int) (Cache[K, V], error) {
 	l, err := lru.New[K, V](size)
 	if err != nil {
 		return nil, err
 	}
 	return &InMemoryLRUCache[K, V]{l: l}, nil
+}
+
+type RedisCache[V any] struct {
+	rdb redis.Client
+}
+
+func (c *RedisCache[V]) Get(ctx context.Context, key string) (Maybe[V], error) {
+	raw, err := c.rdb.Get(ctx, key).Result()
+	if err != nil {
+		if errors.Is(err, redis.Nil) {
+			return Maybe[V]{Found: false}, nil
+		}
+		return Maybe[V]{Found: false}, err
+	}
+
+	var v V
+	err = json.UnmarshalContext(ctx, []byte(raw), &v)
+	if err != nil {
+		return Maybe[V]{Found: false}, err
+	}
+
+	return Maybe[V]{Value: v, Found: true}, nil
+}
+
+func (c *RedisCache[V]) Set(ctx context.Context, key string, value V) error {
+	b, err := json.MarshalContext(ctx, value)
+	if err != nil {
+		return err
+	}
+
+	err = c.rdb.Set(ctx, key, b, 0).Err()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *RedisCache[V]) Delete(ctx context.Context, key string) error {
+	err := c.rdb.Del(ctx, key).Err()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *RedisCache[V]) Clear(ctx context.Context) error {
+	return c.rdb.FlushAll(ctx).Err()
+}
+
+func NewRedisCache[V any](rdb redis.Client) Cache[string, V] {
+	return &RedisCache[V]{rdb: rdb}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module github.com/harsssh/isucon-utility
 
 go 1.23.4
 
-require github.com/hashicorp/golang-lru/v2 v2.0.7 // indirect
+require (
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/goccy/go-json v0.10.3 // indirect
+	github.com/hashicorp/golang-lru/v2 v2.0.7 // indirect
+	github.com/redis/go-redis/v9 v9.7.0 // indirect
+)
