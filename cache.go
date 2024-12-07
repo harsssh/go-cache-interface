7a68a31b7af0ddf83286fe395125bca340407269
Kentaro Mizuki <66548698+harsssh@users.noreply.github.com>
2024-12-07 19:18:36 +0900

    flatten

--- cache.go
+++ cache.go
@@ -1,11 +1,59 @@
-package cache
+package isuutil
 
 import (
 	"context"
 	"errors"
 	"github.com/goccy/go-json"
+	lru "github.com/hashicorp/golang-lru/v2"
 	"github.com/redis/go-redis/v9"
 )
+
+type Maybe[V any] struct {
+	Value V
+	Found bool
+}
+
+type Cache[K comparable, V any] interface {
+	Get(ctx context.Context, key K) (Maybe[V], error)
+	Set(ctx context.Context, key K, value V) error
+	Delete(ctx context.Context, key K) error
+	Clear(ctx context.Context) error
+}
+
+type inMemoryLRUCache[K comparable, V any] struct {
+	l *lru.Cache[K, V]
+}
+
+func (c *inMemoryLRUCache[K, V]) Get(ctx context.Context, key K) (Maybe[V], error) {
+	v, ok := c.l.Get(key)
+	if !ok {
+		return Maybe[V]{Found: false}, nil
+	}
+	return Maybe[V]{Value: v, Found: true}, nil
+}
+
+func (c *inMemoryLRUCache[K, V]) Set(ctx context.Context, key K, value V) error {
+	c.l.Add(key, value)
+	return nil
+}
+
+func (c *inMemoryLRUCache[K, V]) Delete(ctx context.Context, key K) error {
+	c.l.Remove(key)
+	return nil
+}
+
+func (c *inMemoryLRUCache[K, V]) Clear(ctx context.Context) error {
+	c.l.Purge()
+	return nil
+}
+
+func NewInMemoryLRUCache[K comparable, V any](size int) (Cache[K, V], error) {
+	l, err := lru.New[K, V](size)
+	if err != nil {
+		return nil, err
+	}
+	return &inMemoryLRUCache[K, V]{l: l}, nil
+}
 
 type redisCache[V any] struct {
 	rdb redis.Client
